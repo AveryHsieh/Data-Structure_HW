ca8dedb75a6e7f780c1257a66c2aa2516e7ac685
Avery Hsieh <a28680535@gmail.com>
2022-10-13 12:50:53 +0800

    DS_Test

--- main.cpp
+++ main.cpp
@@ -49,7 +49,7 @@ void vectorTest() {
         //double addAvrgSpendTime = addTotalSpendTime / repeat;
         double sumAvrgSpendTime = sumTotalSpendTime / repeat;
         //cout << endl << "Dynamic Array新增2^" << k << "個隨機數所需的時間:" << addAvrgSpendTime << " sec" << endl;
-        cout << endl << "Dynamic Array新增2^" << k << "個隨機數後後access所有資料所需的時間:" << sumAvrgSpendTime << " sec" << endl << endl;
+        cout << endl << "Dynamic Array新增2^" << k << "個隨機數後後access所有資料所需的時間: " << sumAvrgSpendTime << " sec" << endl << endl;
     }
 
 }
@@ -94,7 +94,7 @@ void listTest() {
         //double addAvrgSpendTime = addTotalSpendTime / repeat;
         double sumAvrgSpendTime = sumTotalSpendTime / repeat;
         //cout << endl << "Linked List新增2^" << k << "個隨機數所需的時間:" << addAvrgSpendTime << " sec" << endl;
-        cout << endl << "Linked List新增2^" << k << "個隨機數後access所有資料所需的時間:" << sumAvrgSpendTime << " sec" << endl << endl;
+        cout << endl << "Linked List新增2^" << k << "個隨機數後access所有資料所需的時間: " << sumAvrgSpendTime << " sec" << endl << endl;
     }
 
 }
